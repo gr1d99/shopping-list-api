a55610c73f49e941b2db09efe0bde84288fb32f3
gr1d99 <gideonkimutai9@gmail.com>
2017-10-05 14:12:52 +0300

    [TESTS] Add failling user model class test

--- tests/test_user.py
+++ tests/test_user.py
@@ -2,3 +2,22 @@
 
 """Contains all Tests for system app user"""
 
+import unittest
+from main import app
+from web_app.conf import app_config
+from web_app.db.models import User
+
+class TestUser(unittest.TestCase):
+    """tests main User model class"""
+
+    def setUp(self):
+        app.config.from_object(app_config.TestingConfig)
+        self.user = User
+
+    def test_user_create(self):
+        """
+        test if a user is created successfully
+        """
+        self.user(username='gideon',
+                  password='gideonpassword',
+                  email='gideonkimutai9@gmail.com')
