cb1b8b784c3e74ed66569d46b9988b7aef500956
gr1d99 <gideonkimutai9@gmail.com>
2017-10-06 13:39:51 +0300

    Hook bcrypt to flask instace

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from web_app.conf import app_config
 
 app = Flask(__name__)
 app.config.from_object(app_config.DevelopmentConfig)
+bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 
 
