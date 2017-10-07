0056cad7ee98cd52295bab65cf13dd72b1f689c3
gr1d99 <gideonkimutai9@gmail.com>
2017-10-07 09:11:16 +0300

    [FEATURE]Make user user model inherit attributes from base user manager class

--- web_app/db/models.py
+++ web_app/db/models.py
@@ -6,15 +6,24 @@
 """
 
 from main import db
+from .base import BaseUserManager
 
 
-class User(db.Model):
+class User(BaseUserManager, db.Model):
     ___tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(30), unique=True, nullable=False)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = self.hash_password(password)
+        self.email = self.normalize_email(email)
+
+    def verify_password(self, password):
+        return self._verify_password(password)
 
     def __repr__(self):
         return '<User %r>' % self.username
