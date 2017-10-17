ca8eb5b776cfe420882407a470fa9f8a12286e57
gr1d99 <gideonkimutai9@gmail.com>
2017-10-17 18:36:39 +0300

    Change config to testing

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 from web_app.conf import app_config
 
 app = Flask(__name__)
-app.config.from_object(app_config.DevelopmentConfig)
 bcrypt = Bcrypt(app)
+app.config.from_object(app_config.TestingConfig)
 db = SQLAlchemy(app)
 
 
