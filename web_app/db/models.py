954a2825b22790695952b486461c701dab9ee203
gr1d99 <gideonkimutai9@gmail.com>
2017-10-18 20:39:30 +0300

    Add authentication attributes
    Introduced is_authenticated column which is a boolean. Thus if the user is authenticated the value will be updated to True and False when the user logs out.
    Added a authenticate method which will set user as authenticated.

--- web_app/db/models.py
+++ web_app/db/models.py
@@ -8,6 +8,7 @@
 """
 from main import db
 from web_app.utils.date_helpers import datetime
+from web_app.utils.callables import CallableFalse, CallableTrue
 from .base import BaseUserManager, BaseModel
 
 
@@ -19,6 +20,7 @@ class User(BaseUserManager, BaseModel,  db.Model):
     email = db.Column(db.String(30), unique=True, nullable=False)
     shopping_lists = db.relationship('ShoppingList', backref='user',
                                      lazy='dynamic', cascade='all, delete-orphan')
+    is_authenticated = db.Column(db.Boolean, default=CallableFalse)
     date_joined = db.Column(db.DateTime, default=datetime.now())
 
     def __init__(self, username, password, email, date_joined=None):
@@ -26,6 +28,11 @@ class User(BaseUserManager, BaseModel,  db.Model):
         self.password = self.hash_password(password)
         self.email = self.normalize_email(email)
         self.date_joined = date_joined
+
+    def authenticate(self):
+        self.is_authenticated = CallableFalse
+        self.save()
+        return CallableTrue
 
     def verify_password(self, password):
         return self._verify_password(password)
