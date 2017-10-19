e04eaa133670815fcd80a6ef4d4d3243186bb1da
gr1d99 <gideonkimutai9@gmail.com>
2017-10-19 14:04:45 +0300

    [Feature]Initialized app api

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_bcrypt import Bcrypt
+from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from web_app.conf import app_config
 
@@ -9,7 +10,9 @@ app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.config.from_object(app_config.TestingConfig)
 db = SQLAlchemy(app)
+api = Api(app, prefix="/api/v1/")
 
+from web_app.db import models
 
 if __name__ == '__main__':
     app.run()
