79b3a96bc467b2ad9650246a6bf0a6c9bda4a4cb
gr1d99 <gideonkimutai9@gmail.com>
2017-10-20 14:00:13 +0300

    [Tests]Add more tests for user api through endpoints

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -16,13 +16,16 @@
 
 import collections
 import json
+from ddt import ddt, file_data
 
-from flask import jsonify
 from main import db
 from web_app.conf import app_config
+from web_app.db.models import User
+from web_app.db.utils.messages import account_created
 from .base import TestBase
 
 
+@ddt
 class TestUserAuth(TestBase):
     """
     Test class for app user authentication funtionalities.
@@ -48,6 +51,8 @@ class TestUserAuth(TestBase):
         self.base_url_prefix = '/api/v1/'
         self.content_type = 'application/json'
         self.user_info = _user_info
+        self.register_url = self.base_url_prefix + 'auth/register'
+        self.login_url = self.base_url_prefix + 'auth/login'
 
     def tearDown(self):
         """
@@ -56,85 +61,405 @@ class TestUserAuth(TestBase):
         db.session.remove()
         db.drop_all()
 
-    def test_user_can_register(self):
+    def register_user(self, username, password, email):
+        """
+        Helper method to register a user.
+
+        Makes post request using user information initialized in
+        the setUp method.
+        """
+        resp = self.client.post(self.register_url,
+                                data=json.dumps(dict(username=username,
+                                                     email=email,
+                                                     password=password)),
+                                content_type=self.content_type)
+        return resp
+
+    def login_user(self, username, password):
+        """
+        Helper method to login user.
+
+        Makes post request using user information initialized in
+        the setUp method.
+        """
+        resp = self.client.post(self.login_url,
+                                data=json.dumps(dict(username=username,
+                                                     password=password)),
+                                content_type=self.content_type)
+        return resp
+
+    @file_data("test_data/valid_userinfo.json")
+    def test_user_can_register(self, info):
         """
         Testing call to /auth/register/.
         """
-        # register url
-        url = self.base_url_prefix + 'auth/register/'
-        resp = self.client.post(url,
-                                data=json.dumps(dict(username=self.user_info.username,
-                                                     email=self.user_info.email,
-                                                     password=self.user_info.password)),
-                                content_type=self.content_type)
+
+        # register user
+        resp = self.register_user(username=info[0],
+                                  password=info[1],
+                                  email=info[2])
 
         # returned data
-        rtn_data = resp.get_data()
+        rtn_data = resp.data
 
         self.assertTrue(resp.status_code == 201)  # status_code 210 created.
 
         # username and email we used to create user should be in
         # returned data.
-        self.assertIn(self.user_info.username, rtn_data)
-        self.assertIn(self.user_info.email, rtn_data)
+        self.assertIn(account_created, rtn_data)
 
-    def test_user_can_login(self):
+    @file_data("test_data/valid_userinfo.json")
+    def test_authenticated_login(self, info):
+        """
+        Test user cannot login again if authenticated.
+        """
+        # register user first
+        # register
+        reg_resp = self.register_user(username=info[0],
+                                      password=info[1],
+                                      email=info[2])
+
+        # confirm registration response
+        self.assertTrue(reg_resp.status_code == 201)
+        self.assertIn(account_created, reg_resp.data)
+
+        # login user
+        first_resp = self.login_user(username=info[0],
+                                     password=info[1])
+
+        self.assert200(first_resp)
+        self.assertIn('Logged in', first_resp.data)
+
+        # call make request to login again
+        second_resp = self.login_user(username=info[0],
+                                      password=info[1])
+
+        self.assert200(second_resp)
+        self.assertIn('Already logged in', second_resp.data)
+
+    def test_registration_without_username(self):
+        """
+        Test registration without username.
+        """
+        expected_error_message = 'Provide a username'
+
+        reg_resp = self.client.post(self.register_url,
+                                    data=json.dumps(dict(email=self.user_info.email,
+                                                         password=self.user_info.password)),
+                                    content_type=self.content_type)
+
+        self.assertIn(expected_error_message, reg_resp.get_data())
+        self.assert400(reg_resp)
+
+    def test_register_without_email(self):
+        """
+        Test registration without email.
+        """
+        expected_error_message = 'Provide an email'
+        reg_url = self.base_url_prefix + 'auth/register'
+        reg_resp = self.client.post(reg_url,
+                                    data=json.dumps(dict(username=self.user_info.username,
+                                                         password=self.user_info.password)),
+                                    content_type=self.content_type)
+
+        self.assertIn(expected_error_message, reg_resp.get_data())
+        self.assert400(reg_resp)
+
+    def test_register_without_password(self):
+        """
+        Test registration without password.
+        """
+        expected_error_message = 'Provide a password'
+        reg_resp = self.client.post(self.register_url,
+                                    data=json.dumps(dict(username=self.user_info.username,
+                                                         email=self.user_info.email)),
+                                    content_type=self.content_type)
+
+        self.assertIn(expected_error_message, reg_resp.get_data())
+        self.assert400(reg_resp)
+
+    @file_data("test_data/valid_userinfo.json")
+    def test_register_with_existing_username(self, info):
+        """
+        Test registration with existing username.
+        """
+        # register user first
+        # register user
+        self.register_user(username=info[0],
+                           password=info[1],
+                           email=info[2])
+
+        second_reg_resp = self.register_user(username=info[0],
+                                             password=info[1],
+                                             email="different@gmail.com")
+
+        self.assert400(second_reg_resp)
+        self.assertIn('User with that username exists', second_reg_resp.data)
+
+    @file_data("test_data/valid_userinfo.json")
+    def test_register_with_existing_email(self, info):
+        """
+        Test registration with existing email.
+        """
+        # register user first
+        # register
+        self.register_user(username=info[0],
+                           password=info[1],
+                           email=info[2])
+
+        second_reg_resp = self.register_user(username="newusername",
+                                             password=info[1],
+                                             email=info[2])
+
+        self.assert400(second_reg_resp)
+        self.assertIn('User with that email exists', second_reg_resp.data)
+
+    def test_login_without_username(self):
+        """A user should not be able to login without a username."""
+        # login
+        first_resp = self.client.post(self.login_url,
+                                      data=json.dumps(dict(password=self.user_info.password)),
+                                      content_type=self.content_type)
+
+        self.assert400(first_resp)
+        self.assertIn('Provide a username', first_resp.data)
+
+    def test_login_without_password(self):
+        """A user should not be able to login without a password."""
+        # login
+        first_resp = self.client.post(self.login_url,
+                                      data=json.dumps(dict(username=self.user_info.email)),
+                                      content_type=self.content_type)
+
+        self.assert400(first_resp)
+        self.assertIn('Provide a password', first_resp.data)
+
+    def test_invalid_cridentials(self):
+        """Test invalid cridentials provided by user."""
+        # login
+        resp = self.client.post(self.login_url,
+                                data=json.dumps(dict(username="someuser",
+                                                     password="somepassword")),
+                                content_type=self.content_type)
+
+        self.assert401(resp)
+        self.assertIn('Incorrect username or password!!', resp.data)
+
+    @file_data("test_data/valid_userinfo.json")
+    def test_user_can_login(self, info):
         """
         Test call to /auth/login/
         """
 
         # register user first
-        # register url
-        reg_url = self.base_url_prefix + 'auth/register/'
-        reg_resp = self.client.post(reg_url,
-                                    data=json.dumps(dict(username=self.user_info.username,
-                                                         email=self.user_info.email,
-                                                         password=self.user_info.password)),
-                                    content_type=self.content_type)
+        # register
+        reg_resp = self.register_user(username=info[0],
+                                      password=info[1],
+                                      email=info[2])
 
         # confirm registration response
         self.assertTrue(reg_resp.status_code == 201)
-        self.assertIn(self.user_info.username, reg_resp.get_data())
-        self.assertIn(self.user_info.email, reg_resp.get_data())
+        self.assertIn(account_created, reg_resp.data)
 
         # login user
-        # login url
-        login_url = self.base_url_prefix + 'auth/login/'
-        resp = self.client.post(login_url,
-                                data=dict(username=self.user_info.username,
-                                          password=self.user_info.password),
-                                content_type=self.content_type)
-        self.assertTrue(resp.status_code == 200)
+        # login
+        resp = self.login_user(username=info[0], password=info[1])
 
-    def test_logout(self):
-        """
-        Test call to /auth/logout/
-        """
-        url = self.base_url_prefix + 'auth/logout/'
+        self.assert200(resp)
+        self.assertIn('Logged in', resp.data)
+
+    @file_data("test_data/valid_userinfo.json")
+    def test_view_account_details(self, info):
+        """Test user can view account details."""
 
         # register user first
-        # register url
-        reg_url = self.base_url_prefix + 'auth/register/'
-        reg_resp = self.client.post(reg_url,
-                                    data=json.dumps(dict(username=self.user_info.username,
-                                                         email=self.user_info.email,
-                                                         password=self.user_info.password)),
-                                    content_type=self.content_type)
+        # register
+        reg_resp = self.register_user(username=info[0],
+                                      password=info[1],
+                                      email=info[2])
 
         # confirm registration response
         self.assertTrue(reg_resp.status_code == 201)
-        self.assertIn(self.user_info.username, reg_resp.get_data())
-        self.assertIn(self.user_info.email, reg_resp.get_data())
+        self.assertIn(account_created, reg_resp.data)
 
         # login user
-        # login url
-        login_url = self.base_url_prefix + 'auth/login/'
-        login_resp = self.client.post(login_url,
-                                      data=dict(username=self.user_info.username,
-                                                password=self.user_info.password),
-                                      content_type=self.content_type)
-        self.assertTrue(login_resp.status_code == 200)
+        # login
+        resp = self.login_user(username=info[0], password=info[1])
 
-        # make request to logout
-        logout_resp = self.client.get(url)
-        self.assertTrue(logout_resp.status_code == 200)
+        self.assert200(resp)
+        self.assertIn('Logged in', resp.data)
+
+        # view details url
+        get_url = self.base_url_prefix + 'auth/%(username)s' \
+                                         % dict(username=info[0])
+
+        view_resp = self.client.get(get_url, content_type=self.content_type)
+
+        user = User.query.filter_by(username=info[0]).first()
+
+        # format date_joined to string.
+        date_joined = user.date_joined.strftime("%a, %d %b %Y")
+        self.assert200(view_resp)
+        self.assertIn(user.username, view_resp.data)
+        self.assertIn(user.email, view_resp.data)
+        self.assertIn(date_joined, view_resp.data)
+
+    @file_data("test_data/valid_userinfo.json")
+    def test_update_user_details(self, info):
+        """Test user can update details."""
+        # register user first
+        # register
+        reg_resp = self.register_user(username=info[0],
+                                      password=info[1],
+                                      email=info[2])
+
+        # confirm registration response
+        self.assertTrue(reg_resp.status_code == 201)
+        self.assertIn(account_created, reg_resp.data)
+
+        # login user
+        # login
+        resp = self.login_user(username=info[0], password=info[1])
+
+        self.assert200(resp)
+        self.assertIn('Logged in', resp.data)
+
+        # make update request
+        update_url = self.base_url_prefix + 'auth/%(username)s' \
+                                            % dict(username=info[0])
+
+        new_details = {
+            'username': 'newusername',
+            'password': 'newpassword',
+            'email': 'new@email.com'
+        }
+
+        data = json.dumps(new_details)
+
+        update_resp = self.client.put(update_url,
+                                      data=data,
+                                      content_type=self.content_type)
+
+        self.assert200(update_resp)
+        self.assertIn("Account updated", update_resp.data)
+
+    @file_data("test_data/valid_userinfo.json")
+    def test_unautheticated_update_request(self, info):
+        """Test cannot update account without being authenticated."""
+        # register user first
+        # register
+        reg_resp = self.register_user(username=info[0],
+                                      password=info[1],
+                                      email=info[2])
+
+        # confirm registration response
+        self.assertTrue(reg_resp.status_code == 201)
+        self.assertIn(account_created, reg_resp.data)
+
+        # update url
+        update_url = self.base_url_prefix + 'auth/%(username)s' \
+                                            % dict(username=info[0])
+
+        new_details = {
+            'username': 'updatedusername',
+            'password': 'updatedpassword',
+            'email': 'updated@email.com'
+        }
+
+        data = json.dumps(new_details)
+
+        update_resp = self.client.put(update_url,
+                                      data=data,
+                                      content_type=self.content_type)
+
+        self.assert401(update_resp)
+        self.assertIn("Login first", update_resp.data)
+
+    @file_data("test_data/valid_userinfo.json")
+    def test_update_with_none_existing_user(self, info):
+        """Test non-existing user cannot update."""
+        # update url
+        update_url = self.base_url_prefix + 'auth/%(username)s' \
+                                            % dict(username=info[0])
+
+        new_details = {
+            'username': 'updatedusername',
+            'password': 'updatedpassword',
+            'email': 'updated@email.com'
+        }
+
+        data = json.dumps(new_details)
+
+        update_resp = self.client.put(update_url,
+                                      data=data,
+                                      content_type=self.content_type)
+
+        self.assert401(update_resp)
+        self.assertIn("Create account or login", update_resp.data)
+
+    @file_data("test_data/valid_userinfo.json")
+    def test_update_with_existing_data(self, info):
+        """Test cannot update account with pre-existing data."""
+        # register user first
+        # register first user
+        reg_resp = self.register_user(username=info[0],
+                                      password=info[1],
+                                      email=info[2])
+
+        # register second user
+        target_username = 'existingusername'
+        target_email = "existingemail@email.com"
+        reg_resp2 = self.register_user(username=target_username,
+                                       password=info[1],
+                                       email=target_email)
+
+        # confirm registration responses
+        self.assertTrue(reg_resp.status_code == 201)
+        self.assertIn(account_created, reg_resp.data)
+
+        self.assertTrue(reg_resp.status_code == 201)
+        self.assertIn(account_created, reg_resp2.data)
+
+        # login first user
+        # login
+        resp = self.login_user(username=info[0], password=info[1])
+
+        self.assert200(resp)
+        self.assertIn('Logged in', resp.data)
+
+        # update url
+        update_url = self.base_url_prefix + 'auth/%(username)s' \
+                                            % dict(username=info[0])
+
+        new_details2 = {
+            'username': target_username,  # same as reg_resp2 username
+            'password': 'somepassword',
+            'email': 'new_user1@email.com'
+        }
+
+        new_details3 = {
+            'username': "new_detailuser",
+            'password': 'updatedpassword',
+            'email': target_email  # same as reg2_resp email
+        }
+
+        # PUT data
+        data2 = json.dumps(new_details2)
+        data3 = json.dumps(new_details3)
+
+        # make a PUT request
+        update_det2 = self.client.put(update_url,
+                                      data=data2,
+                                      content_type=self.content_type)
+
+        update_det3 = self.client.put(update_url,
+                                      data=data3,
+                                      content_type=self.content_type)
+
+        # assertions
+        self.assert400(update_det2)
+        self.assert400(update_det3)
+        self.assertIn("User with %(uname)s exists" %
+                      dict(uname=target_username), update_det2.data)
+        self.assertIn("User with %(email)s exists" %
+                      dict(email=target_email), update_det3.data)
