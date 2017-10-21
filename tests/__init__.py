d0f80a7a6140a5dfc37f5b975c9bc9f8d9ea6ca6
gr1d99 <gideonkimutai9@gmail.com>
2017-10-21 23:14:01 +0300

    [Feature]Import all objects to be used by tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,8 @@
-from main import app
+from main import APP
 from web_app.conf import app_config
 from .base import TestBase
 
 
 __all__ = [
-    app, app_config, TestBase
+    APP, app_config, TestBase
 ]
