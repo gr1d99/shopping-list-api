aabc344e9898e7c9595fea8d9d90bca790b7cf77
gr1d99 <gideonkimutai9@gmail.com>
2017-10-27 09:56:42 +0300

    Remove unused import and move blacklist token callback method to a different file

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@
 Contains application main instances and configurations.
 """
 
-from flask import Blueprint, Flask
+from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_httpauth import HTTPBasicAuth
 from flask_jwt_extended import JWTManager
@@ -20,18 +20,7 @@ AUTH = HTTPBasicAuth()
 API = Api(APP, prefix="/api/v1/")
 jwt = JWTManager(APP)
 
-from .models import BlacklistToken
-
-@jwt.token_in_blacklist_loader
-def check_if_token_in_blacklist(decrypted_token):
-    jti = decrypted_token['jti']
-    token = BlacklistToken.query.filter_by(token=jti).first()
-    if token:
-        return True
-
-    else:
-        return False
-
+from app.auth import security
 from app.auth.urls import urls
 
 for url in urls:
