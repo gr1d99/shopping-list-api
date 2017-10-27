611d49f17a85eb2dac3745a69a2bda9ec44171c3
gr1d99 <gideonkimutai9@gmail.com>
2017-10-27 09:57:31 +0300

    Add blacklist tokens callback function

--- app/auth/security.py
+++ app/auth/security.py
@@ -1,1 +1,13 @@
+from app import jwt
+from ..models import BlacklistToken
 
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    token = BlacklistToken.query.filter_by(token=jti).first()
+    if token:
+        return True
+
+    else:
+        return False
