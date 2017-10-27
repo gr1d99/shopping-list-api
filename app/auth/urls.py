a3f855a1f3666dd3b6cbdd9204d83ca8a3d1ac08
gr1d99 <gideonkimutai9@gmail.com>
2017-10-27 17:05:30 +0300

    [Fix]Update blueprint name of module to resolve blueprint naming conflicts

--- app/auth/urls.py
+++ app/auth/urls.py
@@ -1,13 +1,26 @@
 import collections
 
-from .views import RefreshTokenApi, UserRegisterApi, UserLoginApi, UserLogoutApi, UserProfileApi
+from flask import Blueprint
+
+from app import API
+
+auth_blueprint = Blueprint('auth', __name__)
+
+
+from .views import (RefreshTokenApi, ResetPasswordApi, UserRegisterApi,
+                    UserLoginApi, UserLogoutApi, UserProfileApi)
 
 url = collections.namedtuple('url', ['route', 'resource'])
 
-urls = [
+auth_urls = [
     url('auth/users', UserProfileApi),
     url('auth/register', UserRegisterApi),
     url('auth/login', UserLoginApi),
     url('auth/logout', UserLogoutApi),
     url('auth/refresh-token', RefreshTokenApi),
+    url('auth/reset-password', ResetPasswordApi),
 ]
+
+
+for url in auth_urls:
+    API.add_resource(url.resource, url.route)
