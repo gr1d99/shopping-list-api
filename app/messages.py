182023535bf332a4e7fb7d87c0013036650d73fb
gr1d99 <gideonkimutai9@gmail.com>
2017-10-27 17:12:54 +0300

    Add more messages to be used in api endpoints.

--- app/messages.py
+++ app/messages.py
@@ -4,3 +4,12 @@ password_not_provided = 'Provide a password'
 username_exists = 'User with that username exists, use a different name or login if it belongs to you'
 email_exists = 'User with that email exists, use a different email id or login if it belongs to you'
 account_created = 'Account created, login with your email and password to get your access tokens'
+password_changed = 'Your password has been successfully changed'
+username_or_email_required = 'You must provide a username or email'
+incorrect_old_password = 'Incorrect password provided'
+passwords_donot_match = 'Passwords do not match'
+incorrect_password_or_username = 'Incorrect username or password'
+
+
+# shopping list and shopping items endpoints messages
+shopping_list_created = "Shopping created"
