6c8dab4bc167cbda0de02bd9f628f676264d1b43
gr1d99 <gideonkimutai9@gmail.com>
2017-10-27 05:35:44 +0300

    [Feature]Add flask_jwt to manage token based authentication

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ Contains application main instances and configurations.
 from flask import Blueprint, Flask
 from flask_bcrypt import Bcrypt
 from flask_httpauth import HTTPBasicAuth
+from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from app.conf import app_config
@@ -17,6 +18,19 @@ APP.config.from_object(app_config.DevelopmentConfig)
 DB = SQLAlchemy(APP)
 AUTH = HTTPBasicAuth()
 API = Api(APP, prefix="/api/v1/")
+jwt = JWTManager(APP)
+
+from .models import BlacklistToken
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    token = BlacklistToken.query.filter_by(token=jti).first()
+    if token:
+        return True
+
+    else:
+        return False
 
 from app.auth.urls import urls
 
