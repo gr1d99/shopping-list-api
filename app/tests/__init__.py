21143bca3fe8ccce5a04fbe35acdd9d053128ca2
gr1d99 <gideonkimutai9@gmail.com>
2017-10-27 10:05:18 +0300

    Add refresh token url for testing

--- app/tests/__init__.py
+++ app/tests/__init__.py
@@ -11,12 +11,14 @@ LOGOUT_PREFIX = "auth/logout"
 REGISTER_PREFIX = "auth/register"
 USER_DETAILS_PREFIX = "auth/users"
 UPDATE_USER_DETAILS_PREFIX = "auth/users"
+REFRESH_TOKEN_PREFIX = "auth/refresh-token"
 
 LOGIN_URL = "%(prefix)s%(next)s" % dict(prefix=BASE_PREFIX, next=LOGIN_PREFIX)
 LOGOUT_URL = "%(prefix)s%(next)s" % dict(prefix=BASE_PREFIX, next=LOGOUT_PREFIX)
 REGISTER_URL = "%(prefix)s%(next)s" % dict(prefix=BASE_PREFIX, next=REGISTER_PREFIX)
 USER_DETAILS_URL = "%(prefix)s%(next)s" % dict(prefix=BASE_PREFIX, next=USER_DETAILS_PREFIX)
 UPDATE_USER_DETAILS_URL = "%(prefix)s%(next)s" % dict(prefix=BASE_PREFIX, next=UPDATE_USER_DETAILS_PREFIX)
+REFRESH_USER_TOKEN_URL = "%(prefix)s%(next)s" % dict(prefix=BASE_PREFIX, next=REFRESH_TOKEN_PREFIX)
 
 INVALID_EMAIL_ERR = "Not a valid email address."
 REQUIRED_FIELDS_ERR = "Missing data for required field."
@@ -24,6 +26,7 @@ REQUIRED_FIELDS_ERR = "Missing data for required field."
 
 __all__ = [
     APP, account_created, app_config,
-    CONTENT_TYPE, DB, INVALID_EMAIL_ERR, LOGIN_URL, LOGOUT_URL, REGISTER_URL,
-    REQUIRED_FIELDS_ERR, User, UPDATE_USER_DETAILS_URL, USER_DETAILS_URL,
+    CONTENT_TYPE, DB, INVALID_EMAIL_ERR, LOGIN_URL, LOGOUT_URL,
+    REFRESH_USER_TOKEN_URL, REGISTER_URL, REQUIRED_FIELDS_ERR,
+    User, UPDATE_USER_DETAILS_URL, USER_DETAILS_URL,
 ]
