227209917849f89dca29551535ce520c72fd1c48
gr1d99 <gideonkimutai9@gmail.com>
2017-10-28 09:00:22 +0300

    Update base directory settings

--- app/conf/settings.py
+++ app/conf/settings.py
@@ -1,5 +1,5 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 USE_TZ = True
 TIME_ZONE = 'Africa/Nairobi'
