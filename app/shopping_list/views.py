0c1b87219020366b6096cc978ea8f7931c6044ce
gr1d99 <gideonkimutai9@gmail.com>
2017-10-28 09:13:47 +0300

    [Feature]Add resources to process create, retrieve and update shoppinglists.

--- app/shopping_list/views.py
+++ app/shopping_list/views.py
@@ -1,10 +1,15 @@
+from collections import OrderedDict
+
 from flask import jsonify, make_response
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from webargs.flaskparser import use_args
 
-from .utils import shopping_list_args
-from ..messages import shopping_list_created
+from .utils import shoppinglist_args, shoppinglist_update_args
+from ..core.loggers import AppLogger
+from ..messages import \
+    (server_error, shoppinglist_created, shoppinglist_not_found,
+     shoppinglist_name_exists, shoppinglist_updated, valid_integer_required)
 from ..models import User, ShoppingList
 
 
@@ -13,7 +18,7 @@ class CreateShoppingListApi(Resource):
     Resource to create user shopping list.
     """
 
-    @use_args(shopping_list_args)
+    @use_args(shoppinglist_args)
     @jwt_required
     def post(self, data):
         """
@@ -31,19 +36,202 @@ class CreateShoppingListApi(Resource):
                 return make_response(
                     jsonify(dict(
                         status='success',
-                        message=shopping_list_created,
+                        message=shoppinglist_created,
                         data=dict(
+                            id=shl.id,
                             name=shl.name,
-                            created_on=shl.timestamp.strftime("%Y-%m-%d")
+                            created_on=shl.timestamp.strftime("%Y-%m-%d %H:%M:%S")
                         )
                     )), 201
                 )
 
         except Exception as e:
+            AppLogger(self.__class__.__name__).logger.warning(e)
             return make_response(
                 jsonify(
                     dict(
                         status='fail',
                         message='server error, try again'
                     )), 500
+            )
+
+
+class UserShoppingListsApi(Resource):
+    """
+    Resource to handle fetching of specific user shopping lists.
+    """
+
+    @jwt_required
+    def get(self):
+        """
+        Handle GET request and takes user authentication token.
+        """
+
+        try:
+            current_user = get_jwt_identity()
+
+            user = User.query.filter_by(username=current_user).first()
+
+            # create a list which contains shopping list id and shopping list name/
+            user_shopping_list = [{'id': shl.id, 'name': shl.name} for shl in user.shopping_lists.all()]
+
+            return make_response(
+                jsonify(dict(
+                    status='success',
+                    message=dict(
+                        shopping_lists=user_shopping_list
+                    )
+                ))
+            )
+
+        except Exception as e:
+            AppLogger(self.__class__.__name__).logger.warning(e)
+            return make_response(
+                jsonify(dict(
+                    status='fail',
+                    message=server_error
+                )), 500
+            )
+
+
+class UserShoppingListDetailApi(Resource):
+    """
+    Resource to fetch a single shopping list.
+    """
+
+    @jwt_required
+    def get(self, id):
+        try:
+            current_user = get_jwt_identity()
+            try:
+                int(id)
+
+            except ValueError as e:
+                AppLogger(self.__class__.__name__).logger.warning(e)
+                return make_response(
+                    jsonify(dict(
+                        status='fail',
+                        message=valid_integer_required
+                    )), 400
+                )
+
+            user = User.query.filter_by(username=current_user).first()
+
+            try:
+                shopping_list = user.shopping_lists.filter_by(id=int(id)).first_or_404()
+
+            except Exception as e:
+                AppLogger(self.__class__.__name__).logger.warning(e)
+                return make_response(
+                    jsonify(dict(
+                        status='fail',
+                        message=shoppinglist_not_found
+                    )), 404
+                )
+
+            response = OrderedDict(
+                [
+                    ('status', 'success'),
+                    ('message', OrderedDict(
+                        [
+                            ('id', shopping_list.id),
+                            ('owner_id', shopping_list.owner_id),
+                            ('name', shopping_list.name),
+                            ('created_on', shopping_list.timestamp.strftime("%Y-%m-%d %H:%M:%S")),
+                            ('updated_on', shopping_list.updated.strftime("%Y-%m-%d %H:%M:%S"))
+                        ]
+                    )),
+                ]
+            )
+
+            return make_response(
+                jsonify(response), 200
+            )
+
+        except Exception as e:
+            AppLogger(self.__class__.__name__).logger.warning(e)
+            return make_response(
+                jsonify(dict(
+                    status='fail',
+                    message=server_error
+                )), 500
+            )
+
+    @use_args(shoppinglist_update_args)
+    @jwt_required
+    def put(self, args, id):
+        try:
+            current_user = get_jwt_identity()
+            try:
+                # make sure provided id can be converted to an integer.
+                int(id)
+
+            except ValueError as e:
+                AppLogger(self.__class__.__name__).logger.warning(e)
+                return make_response(
+                    jsonify(dict(
+                        status='fail',
+                        message=valid_integer_required
+                    )), 400
+                )
+
+            # new name provided by client
+            new_name = args.get('new_name', None)
+
+            # if name is none there is no need to modify client resource.
+            if not new_name:
+                return make_response(
+                    jsonify(dict(
+                        status='success',
+                    )), 304
+                )
+
+            # check if shopping list with the same name exists.
+            shl = ShoppingList.query.filter_by(name=new_name).first()
+
+            if shl:
+                return make_response(
+                    jsonify(dict(
+                        status='fail',
+                        message=shoppinglist_name_exists
+                    ))
+                )
+
+            # get user instance
+            user = User.query.filter_by(username=current_user).first()
+
+            try:
+                # now update the resource.
+                shopping_list = user.shopping_lists.filter_by(id=int(id)).first_or_404()
+                shopping_list.name = new_name
+                shopping_list.save()
+
+                return make_response(
+                    jsonify(dict(
+                        status='success',
+                        message=shoppinglist_updated,
+                        data=dict(
+                            name=shopping_list.name,
+                            created_on=shopping_list.timestamp.strftime("%Y-%m-%d %H:%M:%S"),
+                            updated_on=shopping_list.updated.strftime("%Y-%m-%d %H:%M:%S")
+                        )
+                    )), 200
+                )
+
+            except Exception as e:
+                AppLogger(self.__class__.__name__).logger.warning(e)
+                return make_response(
+                    jsonify(dict(
+                        status='fail',
+                        message=shoppinglist_not_found
+                    )), 404
+                )
+
+        except Exception as e:
+            AppLogger(self.__class__.__name__).logger.warning(e)
+            return make_response(
+                jsonify(dict(
+                    status='fail',
+                    message=server_error
+                )), 500
             )
