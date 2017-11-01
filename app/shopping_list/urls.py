721929f459b68b63ca992ff0cf2220ca80f1295a
gr1d99 <gideonkimutai9@gmail.com>
2017-11-01 06:27:20 +0300

    Add shoppingitems api endpoints.

--- app/shopping_list/urls.py
+++ app/shopping_list/urls.py
@@ -5,9 +5,11 @@ from app import API
 shopping_list_blueprint = Blueprint('shopping_list', __name__)
 
 
-from .views import CreateShoppingListApi, UserShoppingListsApi, UserShoppingListDetailApi
+from .views import UserShoppingListsApi, UserShoppingListDetailApi, ShoppingItemApi
 
 
 API.add_resource(UserShoppingListsApi, 'shopping-lists'),
 API.add_resource(UserShoppingListDetailApi, 'shopping-lists/<int:id>'),
-API.add_resource(CreateShoppingListApi, 'shopping-lists'),
+API.add_resource(ShoppingItemApi, 'shopping-lists/<int:shoppinglistId>/shopping-items'),
+API.add_resource(ShoppingItemApi, 'shopping-lists/<int:shoppinglistId>/shopping-items/<int:shoppingitemId>',
+                 endpoint='update-shoppingitem'),
