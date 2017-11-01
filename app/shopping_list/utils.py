28f577753988d500c38b584f5787dce419d377ce
gr1d99 <gideonkimutai9@gmail.com>
2017-11-01 06:28:23 +0300

    Add parsers for endpoints post data.

--- app/shopping_list/utils.py
+++ app/shopping_list/utils.py
@@ -4,12 +4,29 @@ from webargs import fields, validate
 
 shoppinglist_args = collections.OrderedDict(
     [
-        ('name', fields.Str(required=True, validate=validate.Length(min=5)))
+        ('name', fields.Str(required=True, validate=validate.Length(min=3)))
     ]
 )
 
 shoppinglist_update_args = collections.OrderedDict(
     [
-        ('new_name', fields.Str(required=False, validate=validate.Length(min=5)))
+        ('new_name', fields.Str(required=False)),
+        ('is_active', fields.Bool(required=False))
+    ]
+)
+
+shoppingitem_create_args = collections.OrderedDict(
+    [
+        ('name', fields.Str(required=True, validate=validate.Length(min=3))),
+        ('price', fields.Decimal(required=True)),
+        ('bought', fields.Bool(required=False))
+    ]
+)
+
+shoppingitem_update_args = collections.OrderedDict(
+    [
+        ('name', fields.Str(required=False)),
+        ('price', fields.Decimal(required=False)),
+        ('bought', fields.Bool(required=False))
     ]
 )
