da6618567996a58c02421ceacbc323798c625c09
gr1d99 <gideonkimutai9@gmail.com>
2017-11-01 06:20:06 +0300

    [Feature]Add methods to get model instances.

--- app/models.py
+++ app/models.py
@@ -57,6 +57,17 @@ class User(BaseUserManager, BaseModel, DB.Model):
         if User.query.filter_by(username=username).first():
             raise UsernameExists
 
+    @staticmethod
+    def get_user(username):
+        """
+        Gets user instance using provided username.
+
+        :param username: user username.
+        :return: instance.
+        """
+
+        return DB.session.query(User).filter_by(username=username).first()
+
     def save(self):
         """
         Override the default save method to enable inspection of required fields
@@ -99,10 +110,24 @@ class ShoppingList(BaseModel, DB.Model):
     owner_id = DB.Column(DB.Integer, DB.ForeignKey('users.id'))
     shopping_items = DB.relationship('ShoppingItem', backref='shopping_list',
                                      lazy='dynamic', cascade='all, delete-orphan')
+    is_active = DB.Column(DB.Boolean, default=True)
     timestamp = DB.Column(DB.DateTime, default=DB.func.current_timestamp())
     updated = DB.Column(
         DB.DateTime, default=DB.func.current_timestamp(),
         onupdate=DB.func.current_timestamp())
+
+    @staticmethod
+    def get(shoppinglistId, ownerId):
+        """
+        Method to get shoppinglist instance by its id and its owner id to
+        ensure other users dont query instances that dont belong to them.
+
+        :param shoppinglistId: shopping list id.
+        :param ownerId: user id.
+        :return: instance.
+        """
+        instance = DB.session.query(ShoppingList).filter_by(id=shoppinglistId, owner_id=ownerId).first()
+        return instance
 
     def __repr__(self):
         return '<%(name)s obj>' % dict(name=self.name.capitalize())
@@ -112,9 +137,38 @@ class ShoppingItem(BaseModel, DB.Model):
     """Model used to store Shopping List items"""
 
     id = DB.Column(DB.Integer, primary_key=True)
-    name = DB.Column(DB.String(100), nullable=False, unique=True)
+    name = DB.Column(DB.String(100), nullable=False)
+    price = DB.Column(DB.Float, nullable=False)
+    bought = DB.Column(DB.Boolean, default=False)
     shoppinglist_id = DB.Column(DB.Integer, DB.ForeignKey('shopping_list.id'))
-    timestamp = DB.Column(DB.DateTime, default=datetime.now())
+    timestamp = DB.Column(DB.DateTime, default=DB.func.current_timestamp())
+    updated = DB.Column(
+        DB.DateTime, default=DB.func.current_timestamp(),
+        onupdate=DB.func.current_timestamp())
+
+    @staticmethod
+    def exists(shl_id, name):
+        """
+        Method to check if shopping item with a given name exists in a given shoppinglist.
+
+        Different shoppinglists may have similar item names but one single shoppinglist
+        may not have more than one item with similar names.
+        .
+        :param shl_id: shopping list id.
+        :param name: shopping item name.
+        :return: Bool
+        """
+
+        # get the specified shoppinglist.
+        shoppinglist = DB.session.query(ShoppingList).filter_by(id=shl_id).first()
+
+        # filter out items by their names.
+        shoppingitem = shoppinglist.shopping_items.filter_by(name=name).first()
+
+        if shoppingitem is None:
+            return False
+
+        return True
 
     def __repr__(self):
         return '<%(name)s obj>' % dict(name=self.name.capitalize())
