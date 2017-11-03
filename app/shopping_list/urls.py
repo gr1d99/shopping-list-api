7e5cff1426cb154712b7807af3786712c3698ee8
gr1d99 <gideonkimutai9@gmail.com>
2017-11-03 07:51:22 +0300

    Add endpoints names.

--- app/shopping_list/urls.py
+++ app/shopping_list/urls.py
@@ -5,11 +5,13 @@ from app import API
 shopping_list_blueprint = Blueprint('shopping_list', __name__)
 
 
-from .views import UserShoppingListsApi, UserShoppingListDetailApi, ShoppingItemApi
+from .views import ShoppingListsApi, ShoppingListDetailApi, ShoppingItemDetailApi, SearchShoppingListApi
 
 
-API.add_resource(UserShoppingListsApi, 'shopping-lists'),
-API.add_resource(UserShoppingListDetailApi, 'shopping-lists/<int:id>'),
-API.add_resource(ShoppingItemApi, 'shopping-lists/<int:shoppinglistId>/shopping-items'),
-API.add_resource(ShoppingItemApi, 'shopping-lists/<int:shoppinglistId>/shopping-items/<int:shoppingitemId>',
-                 endpoint='update-shoppingitem'),
+API.add_resource(ShoppingListsApi, 'shopping-lists', endpoint='shoppinglist_list'),
+API.add_resource(ShoppingListDetailApi, 'shopping-lists/<int:id>', endpoint='shoppinglist_detail'),
+API.add_resource(ShoppingItemDetailApi, 'shopping-lists/<int:shoppinglistId>/shopping-items',
+                 endpoint='shoppingitem_detail'),
+API.add_resource(ShoppingItemDetailApi, 'shopping-lists/<int:shoppinglistId>/shopping-items/<int:shoppingitemId>',
+                 endpoint='shoppingitem_edit'),
+API.add_resource(SearchShoppingListApi, 'shopping-lists/search', endpoint='shoppinglist_search')
