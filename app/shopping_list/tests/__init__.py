731b3e211733e6bc47ad20c333821edd289f0b86
gr1d99 <gideonkimutai9@gmail.com>
2017-11-03 07:40:44 +0300

    Add custom urls for tests.

--- app/shopping_list/tests/__init__.py
+++ app/shopping_list/tests/__init__.py
@@ -8,6 +8,7 @@ PREFIX_ONE = BASE_PREFIX + 'shopping-lists'
 PREFIX_TWO = BASE_PREFIX + 'shopping-lists/'
 
 # shoppingitems urls.
-CREATE_SHOPPINITEMS_URL = PREFIX_TWO + '%(id)s/shopping-items'
+ALL_SHOPPINGITEMS_URL = PREFIX_TWO + 'shopping-items/all'
+CREATE_SHOPPINGITEMS_URL = PREFIX_TWO + '%(id)s/shopping-items'
 UPDATE_SHOPPINGITEMS_URL = PREFIX_TWO + '%(shl_id)s/shopping-items/%(shi_id)s'
 DELETE_SHOPPINGITEMS_URL = PREFIX_TWO + '%(shl_id)s/shopping-items/%(shi_id)s'
