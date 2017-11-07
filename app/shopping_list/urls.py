bbd24f445b0dc59d235b8c38c9988031c9173876
gr1d99 <gideonkimutai9@gmail.com>
2017-11-07 13:30:37 +0300

    Update coding format to follow PEP8 standards.

--- app/shopping_list/urls.py
+++ app/shopping_list/urls.py
@@ -1,17 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+Contains app shoppinglists and shoppingitems url definations.
+"""
+
 from flask import Blueprint
 
 from app import API
 
-shopping_list_blueprint = Blueprint('shopping_list', __name__)
+from .views import \
+    (ShoppingListsApi, ShoppingListDetailApi,
+     ShoppingItemDetailApi, SearchShoppingListApi)
 
+SHOPPINGLIST = Blueprint('shopping_list', __name__)
 
-from .views import ShoppingListsApi, ShoppingListDetailApi, ShoppingItemDetailApi, SearchShoppingListApi
+API.add_resource(
+    ShoppingListsApi, 'shopping-lists', endpoint='shoppinglist_list')
 
+API.add_resource(
+    ShoppingListDetailApi, 'shopping-lists/<int:id>', endpoint='shoppinglist_detail')
 
-API.add_resource(ShoppingListsApi, 'shopping-lists', endpoint='shoppinglist_list'),
-API.add_resource(ShoppingListDetailApi, 'shopping-lists/<int:id>', endpoint='shoppinglist_detail'),
-API.add_resource(ShoppingItemDetailApi, 'shopping-lists/<int:shoppinglistId>/shopping-items',
-                 endpoint='shoppingitem_detail'),
-API.add_resource(ShoppingItemDetailApi, 'shopping-lists/<int:shoppinglistId>/shopping-items/<int:shoppingitemId>',
-                 endpoint='shoppingitem_edit'),
-API.add_resource(SearchShoppingListApi, 'shopping-lists/search', endpoint='shoppinglist_search')
+API.add_resource(
+    ShoppingItemDetailApi, 'shopping-lists/<int:shoppinglistId>/shopping-items',
+    endpoint='shoppingitem_detail')
+
+API.add_resource(
+    ShoppingItemDetailApi, 'shopping-lists/<int:shoppinglistId>/shopping-items/<int:shoppingitemId>',
+    endpoint='shoppingitem_edit')
+
+API.add_resource(
+    SearchShoppingListApi, 'shopping-lists/search', endpoint='shoppinglist_search')
