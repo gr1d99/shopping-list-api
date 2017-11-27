d2e1781f439e7ef31520d04352cf2f88e5bf46fc
Gideon Kimutai <gideonkimutai9@gmail.com>
2017-11-27 10:51:12 +0300

    Update argument parsers for authentication endpoints.

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -6,40 +6,55 @@ Contains authentication resource endpoints argument parsers.
 
 from collections import OrderedDict
 
+from flask_mail import Message
 from webargs import fields, validate
 
-auth_args = {
-    'username': fields.Str(required=True)
-}
+from app import mail, celery
+from .security import generate_token
 
 registration_args = OrderedDict(
     [
-        ('username', fields.Str(required=True)),
-        ('email', fields.Str(message='Email required', required=True, validate=validate.Email())),
-        ('password', fields.Str(required=True, validate=validate.Length(min=6)))
+        ('username', fields.Str(location='form', required=True)),
+        ('email', fields.Str(location='form', message='Email required',
+                             required=True, validate=validate.Email())),
+        ('password', fields.Str(location='form', required=True,
+                                validate=validate.Length(min=6))),
+        ('confirm', fields.Str(location='form', required=True,
+                               validate=validate.Length(min=6)))
     ]
 )
 
 update_account_args = OrderedDict(
     [
-        ('username', fields.Str(required=False)),
-        ('email', fields.Str(required=False, validate=validate.Email())),
-    ]
-)
-
-login_args = OrderedDict(
-    [
-        ('username', fields.Str(required=True)),
-        ('password', fields.Str(required=True))
+        ('username', fields.Str(location='form', required=False)),
+        ('email', fields.Str(location='form', required=False, validate=validate.Email())),
     ]
 )
 
 reset_password_args = OrderedDict(
     [
-        ('username', fields.Str(required=False)),
-        ('email', fields.Str(required=False, validate=validate.Email())),
-        ('old_password', fields.Str(required=True, validate=validate.Length(min=6))),
-        ('new_password', fields.Str(required=True, validate=validate.Length(min=6))),
-        ('confirm', fields.Str(required=True, validate=validate.Length(min=6)))
+        ('username', fields.Str(location='form', required=True)),
+        ('new_password', fields.Str(location='form', required=True, validate=validate.Length(min=6))),
+        ('confirm', fields.Str(location='form', required=True, validate=validate.Length(min=6))),
+        ('reset_token', fields.Str(location='form', required=True))
     ]
 )
+
+delete_account_args = OrderedDict(
+    [
+        ('confirm', fields.Bool(location='query', required=False))
+    ]
+)
+
+
+@celery.task()
+def send_reset_token(user_id):
+    token = generate_token(user_id)
+    # html_msg = """
+    # <h1 align="center">Password Reset</h1>
+    # <p>Hi %(user)s, this is your password reset token %(token)s</p>
+    # """ % dict(user=username, token=token)
+    # msg = Message('', recipients=[email, ])
+    # msg.html = html_msg
+    # return mail.send(msg)
+    return token
