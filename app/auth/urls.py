612732891e6cd22df3df959e0eaf5afb558d305e
Gideon Kimutai <gideonkimutai9@gmail.com>
2017-11-27 10:49:54 +0300

    Remove refresh token endopint route.

--- app/auth/urls.py
+++ app/auth/urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Contains authentication url definations.
+User authentication endpoints definitions.
 """
 
 from flask import Blueprint
@@ -10,12 +10,11 @@ from app import API
 
 auth_blueprint = Blueprint('auth', __name__)
 
-from .views import (RefreshTokenApi, ResetPasswordApi, UserRegisterApi,
+from .views import (ResetPasswordApi, UserRegisterApi,
                     UserLoginApi, UserLogoutApi, UserProfileApi)
 
 API.add_resource(UserProfileApi, 'auth/users', endpoint="user_detail")
 API.add_resource(UserRegisterApi, 'auth/register', endpoint="user_register")
 API.add_resource(UserLoginApi, 'auth/login', endpoint="user_login")
 API.add_resource(UserLogoutApi, 'auth/logout', endpoint="user_logout")
-API.add_resource(RefreshTokenApi, 'auth/refresh-token', endpoint="token_refresh")
 API.add_resource(ResetPasswordApi, 'auth/reset-password', endpoint="password_reset")
