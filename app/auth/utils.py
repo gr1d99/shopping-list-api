450ba33f7066b99ec95f996373512c12d0fecd0d
Gideon Kimutai <gideonkimutai9@gmail.com>
2017-12-07 14:32:41 +0300

    [Chore]Remove unused function

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -9,7 +9,6 @@ from collections import OrderedDict
 
 from webargs import fields, validate
 
-from app import celery
 from .security import generate_token
 
 registration_args = OrderedDict(
@@ -47,16 +46,3 @@ delete_args = OrderedDict(
         ('password', fields.Str(location='form', required=True))
     ]
 )
-
-
-@celery.task()
-def send_reset_token(user_id):
-    token = generate_token(user_id)
-    # html_msg = """
-    # <h1 align="center">Password Reset</h1>
-    # <p>Hi %(user)s, this is your password reset token %(token)s</p>
-    # """ % dict(user=username, token=token)
-    # msg = Message('', recipients=[email, ])
-    # msg.html = html_msg
-    # return mail.send(msg)
-    return token
