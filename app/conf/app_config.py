f285bd6582fafa0f0f013bb581a9dd1147c69eed
Gideon Kimutai <gideonkimutai9@gmail.com>
2017-12-07 14:36:06 +0300

    [Chore]Remove unneeded configurations

--- app/conf/app_config.py
+++ app/conf/app_config.py
@@ -10,26 +10,12 @@ class Config(object):
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
-    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(seconds=3600)
+    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(seconds=360000)
     JWT_HEADER_NAME = 'x-access-token'
     JWT_HEADER_TYPE = None
     HOST = '0.0.0.0'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     JSON_SORT_KEYS = False
-    CELERY_BROKER_URL = 'redis://localhost:6379',
-    CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
-    MAIL_DEBUG = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
-    MAIL_MAX_EMAILS = None
-    MAIL_SUPPRESS_SEND = False
-    MAIL_ASCII_ATTACHMENTS = False
 
 
 class ProductionConfig(Config):
