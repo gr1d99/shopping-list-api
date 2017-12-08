603be3a6c971517ababb0b5370248e83d7df1054
Gideon Kimutai <gideonkimutai9@gmail.com>
2017-12-08 18:54:07 +0300

    Add new endpoints for password reset resources

--- app/auth/urls.py
+++ app/auth/urls.py
@@ -10,11 +10,12 @@ from app import API
 
 auth_blueprint = Blueprint('auth', __name__)
 
-from .views import (ResetPasswordApi, UserRegisterApi,
+from .views import (PasswordResetTokenApi, PasswordResetApi, UserRegisterApi,
                     UserLoginApi, UserLogoutApi, UserProfileApi)
 
 API.add_resource(UserProfileApi, 'auth/users', endpoint="user_detail")
 API.add_resource(UserRegisterApi, 'auth/register', endpoint="user_register")
 API.add_resource(UserLoginApi, 'auth/login', endpoint="user_login")
 API.add_resource(UserLogoutApi, 'auth/logout', endpoint="user_logout")
-API.add_resource(ResetPasswordApi, 'auth/reset-password', endpoint="password_reset")
+API.add_resource(PasswordResetTokenApi, 'auth/reset-password', endpoint="password_token")
+API.add_resource(PasswordResetApi, 'auth/reset-password/process', endpoint="password_reset")
